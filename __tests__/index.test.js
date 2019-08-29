2573f44f53bde995c060bc70976190372f5f4f23
FedirAnichkin <anichkin.fedir@gmail.com>
2019-08-29 21:21:19 +0300

    end of 4 step

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,13 @@
 import fs from 'fs';
 import gendiff from '../src';
 
-test(
-  'first',
-  () => {
-    const before = `${__dirname}/__fixtures__/before.json`;
-    const after = `${__dirname}/__fixtures__/after.json`;
+const currentFormat = [['json'], ['yml'], ['ini']];
+
+test.each(currentFormat)(
+  'main',
+  (format) => {
+    const before = `${__dirname}/__fixtures__/before.${format}`;
+    const after = `${__dirname}/__fixtures__/after.${format}`;
     const expected = fs.readFileSync(`${__dirname}/__fixtures__/expected.txt`, 'utf-8');
     expect(gendiff(before, after)).toBe(expected);
   },
--- src/parsers.js
+++ src/parsers.js
@@ -1,16 +1,18 @@
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 export default (configPath) => {
   const format = path.extname(configPath);
   const data = fs.readFileSync(configPath, 'utf-8');
   let parse;
   if (format === '.json') {
-    // eslint-disable-next-line prefer-destructuring
-    parse = JSON.parse;
+    parse = JSON.parse(data);
   } else if (format === '.yml') {
-    parse = yaml.safeLoad;
+    parse = yaml.safeLoad(data);
+  } else if (format === '.ini') {
+    parse = ini.parse(data);
   }
-  return parse(data);
+  return parse;
 };
